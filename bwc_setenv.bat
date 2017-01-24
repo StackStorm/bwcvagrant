f80b2e6441c146504d2c6ad06fddc056229e194d
Eric Donohue <edonohue@brocade.com>
2017-01-24 12:26:02 -0500

    fix batch quotation

--- bwc_setenv.bat
+++ bwc_setenv.bat
@@ -1,11 +1,11 @@
 
 #BWC Environment Variables
-set ST2HOSTNAME="bwcvagrant"
-set ST2BOX='ubuntu/trusty64'
-set ST2USER="st2admin"
-set ST2PASSWORD="Ch@ngeMe"
-set BWC_LICENSE=""
-set BWC_SUITES=true
-set ST2IPADDRESS="192.168.16.20"
+set "ST2HOSTNAME=bwcvagrant"
+set "ST2BOX=ubuntu/trusty64"
+set "ST2USER=st2admin"
+set "ST2PASSWORD=Ch@ngeMe"
+set "BWC_LICENSE="
+set "BWC_SUITES=true"
+set "ST2IPADDRESS=192.168.16.20"
 
-export BWC_PACKS=""
+set "BWC_PACKS="
